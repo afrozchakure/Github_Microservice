b94cd24ceae94bb0e9cb6b15c86832179afa5688
afrozchakure <aaaanchakure@gmail.com>
2021-09-23 02:07:36 -0700

    Add initial file

---
+++ Github_Microservice/src/main/java/com/p1/GithubMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMicroserviceApplication.class, args);
+	}
+
+}
