dc6d92c9ef6cb1f209203d9b49925a1b7ec5b98e
hemangmonga <hemangmonga@gmail.com>
2021-09-23 03:18:09 -0700

    final_update_hm

--- Github_Microservice/src/main/java/com/p1/GithubMicroserviceApplication.java
+++ Github_Microservice/src/main/java/com/p1/GithubMicroserviceApplication.java
@@ -1,6 +1,7 @@
 package com.p1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
